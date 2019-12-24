92a26029765cc4a02b908e87dc1ac58583544421
asamlhk <asa.choi@gmail.com>
2019-12-24 18:00:04 +0800

    X

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+
+const appRoutes: Routes = [
+  { path: 'page1', component: Page1Component },
+  { path: 'page2', component: Page2Component },
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    Page2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      
+      { enableTracing: false,
+      useHash: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
